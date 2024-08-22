cf98bf1a4351e17b9623c2b1caa2f45e72b3b3f9
Rohit Nayak <rohit77.rn@gmail.com>
2024-08-22 16:18:30 +0530

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '850px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      screens: {
+        'xss': {'min':'200px','max':'310px'},
+        'xs': {'min':'310px','max':'640px'}
+      },
+    },
+  },
+  plugins: [],
+}
+
